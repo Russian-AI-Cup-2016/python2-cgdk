c67239ca091ca923303fc660402ae747bab34c89
SladeThe <sladethe@gmail.com>
2016-11-25 12:23:09 +0300

    Renamed Message.line to Message.lane.

--- model/Message.py
+++ model/Message.py
@@ -1,5 +1,5 @@
 class Message:
-    def __init__(self, line, skill_to_learn, raw_message):
-        self.line = line
+    def __init__(self, lane, skill_to_learn, raw_message):
+        self.lane = lane
         self.skill_to_learn = skill_to_learn
         self.raw_message = raw_message
